9b08165c58c35e2cbb36944192643669d614f42f
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-04-18 15:48:11 +0200

    [WCT?-master][FIX]:clear import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import "./style/main.css"
 import Header from "./layouts/Header";
-import Home from "./views/Home/Home";
 import HomeV2 from "./views/Home/HomeV2";
 const App = () => {
 	return (
